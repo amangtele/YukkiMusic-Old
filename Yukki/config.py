cd7878d139e964dd1105ca6071fe9f9ee6056897
ᴀᴍᴀɴɢ <122607345+amangtele@users.noreply.github.com>
2023-02-16 23:52:58 +0700

    Update config.py

--- Yukki/config.py
+++ Yukki/config.py
@@ -5,7 +5,7 @@ from os import getenv
 from dotenv import load_dotenv
 
 load_dotenv()
-SESSION_NAME = getenv("SESSION_NAME", "session")
+SESSION_NAME = getenv("SESSION_NAME")
 BOT_TOKEN = getenv("BOT_TOKEN")
 API_ID = int(getenv("API_ID"))
 API_HASH = getenv("API_HASH")
